78be98c012e2f6148a819245d1b07c4d76e43717
Murat <144884820+Mikarmk@users.noreply.github.com>
2024-04-25 14:08:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 # Функция для отображения страницы входа
-def login_page():
+def login_page(username_secret, password_secret):
     st.title("Login")
-    username = st.text_input("Enter your username")
-    password = st.text_input("Enter your password", type="password")
+    username_input = st.text_input("Enter your username")
+    password_input = st.text_input("Enter your password", type="password")
     if st.button("Log in"):
-        if username == st.secrets["username"] and password == st.secrets["password"]:
-            st.session_state.username = username
+        if username_input == username_secret and password_input == password_secret:
+            st.session_state.username = username_input
             st.session_state.logged_in = True
             st.success("Login successful! You can now use the chat bot.")
         else:
@@ -41,10 +41,13 @@ def chat_page():
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
 
+# Загрузка секретов из Streamlit Secrets
+secrets = st.secrets["secrets"]
+username = secrets["username"]
+password = secrets["password"]
+
 # Проверка, зарегистрирован ли или авторизован пользователь
 if "logged_in" not in st.session_state or not st.session_state.logged_in:
-    login_page()
-    if "logged_in" not in st.session_state or not st.session_state.logged_in:
-        registration_page()
+    login_page(username, password)
 else:
     chat_page()
